c092eee1aa90217ac37f0843bca4916925c4fcd5
Evan Chan <echan51@stuy.edu>
2024-09-17 10:21:55 -0400

    e

--- 02_cb0/list1/common_end.py
+++
@@ -1,3 +0,0 @@
-def common_end(a, b):
-  return (a[len(a)-1]==b[len(b)-1]) or a[0]==b[0]
-
--- 02_cb0/list1/sum3.py
+++
@@ -1,6 +0,0 @@
-def sum3(nums):
-  sm = 0
-  for i in range(0,len(nums),1):
-    sm+=nums[i]
-  return sm
-

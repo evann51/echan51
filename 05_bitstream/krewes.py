e3bf9dd62852ef759e06be54905503d3a907679f
Evan Chan <echan51@stuy.edu>
2024-09-17 10:58:54 -0400

    comments for bitstream

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -3,33 +3,38 @@
 #SoftDev
 #K05 -- Processing Bitstreams
 #2024-09-17
-#time spent: tbd
+#time spent: .5 hours
 import random as r
 
 f = open("krewes.txt", "r")
 string = f.read()
+
 number = 0
 index = 0
 dict = {}
+
 while (index < len(string) - 1):
+
     pd = ""
     while(not (string[index] == '$')):
         pd += string[index]
         index += 1
-    index += 3
+    index += 3 #"skip" past the $s
 
     devo = ""
     while(not (string[index] == '$')):
         devo += string[index]
         index += 1
-    index += 3
+    index += 3 #"skip" past the $s
 
     ducky = ""
     while(not (string[index] == '@')):
         ducky += string[index]
         index += 1
-    index += 3
-    dict.update({number: [devo, pd, ducky]})
-    number += 1
+
+    dict.update({number: [devo, pd, ducky]}) #adds new student to dict
+    number += 1 #so that next update doesn't overwrite another student
+
+    index += 3 #"skip" past the @s
 
 print(dict[r.randint(0, len(dict) - 1)])

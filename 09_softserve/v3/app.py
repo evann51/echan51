ec70304a6d813fd50fc6d1665666f8b8a075ce4a
Evan Chan <echan51@stuy.edu>
2024-09-24 10:38:26 -0400

    added v3

---
+++ 09_softserve/v3/app.py
@@ -0,0 +1,15 @@
+# Evan Chan
+# SoftDev
+# September 2024
+
+from flask import Flask               #imports Flask
+app = Flask(__name__)                 #create instance of class Flask
+
+@app.route("/")                       #assign fxn to route
+def hello_world():                    #defines new function
+    print("about to print __name__...") #prints in terminal
+    print(__name__)                     #prints in terminal
+    return "No hablo queso!"          #prints "No hablo queso!" on the webpage
+
+app.debug = True        #sets debug to true
+app.run()       #runs app

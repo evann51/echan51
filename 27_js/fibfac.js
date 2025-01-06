54c0192f4a0a4a2caeaa27b1eb082c5f8eeb125c
Evan Chan <echan51@stuy.edu>
2025-01-06 11:01:43 -0500

    e

---
+++ 27_js/fibfac.js
@@ -0,0 +1,34 @@
+//Team KungFuPandaSquad
+//SoftDev pd4
+//K27 - Basic functions in JavaScript
+//2025-01-06m
+
+//JavaScript implementations of Day0 recursive Scheme functions
+
+//factorial:
+var fact = fac(n){
+  if(n<=0){
+    1;
+  }
+  else if(n==1){
+    1;
+  }
+  else return n*fac(n-1);
+}
+//<your team's fact(n) implementation>
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+
+//-----------------------------------------------------------------
+
+
+//fib:
+
+//<your team's fib(n) implementation>
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+//=================================================================
